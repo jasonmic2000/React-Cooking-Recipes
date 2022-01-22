64a852a89b895af4df9e2e51bdbba678fd927497
Jason Michael <jason_mic2000@yahoo.com>
2022-01-22 16:09:35 +0530

    Recipe List Component
    Created RecipeList component, styled it in the form of grid of cards on the home page, with routing to recipe pages with different id's

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,7 +3,8 @@ import { useFetch } from '../../hooks/useFetch'
 // styles
 import './Home.css'
 
-import React from 'react';
+// components
+import RecipeList from '../../components/RecipeList';
 
 export default function Home() {
   const { data, isPending, error } = useFetch('http://localhost:3000/recipes')
@@ -13,9 +14,7 @@ export default function Home() {
       <div className='home'>
           {error && <p className='error'>{error}</p>}
           {isPending && <p className='loading'>Loading...</p>}
-          {data && data.map(recipe => (
-            <h2 key={recipe.id}>{recipe.title}</h2>
-          ))}
+          {data && <RecipeList recipes={data}/>}
       </div>
   )
 }
